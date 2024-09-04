e43c26424dc2a85bf534686b9c8b7b15584b6960
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2024-09-04 13:47:53 +0530

    Hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        customCream: '#FFFAE9',
+        customCream: "#FFFAE9",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+      animation: {
+        marquee: "marquee 10s linear infinite",
+        float: 'float 3s ease-in-out infinite',
       },
       fontFamily: {
         nohemi_l: ["Light", "sans-serif"],
@@ -18,7 +29,10 @@ export default {
         nohemi_b: ["Bold", "sans-serif"],
         nohemi_eb: ["ExtraBold", "sans-serif"],
       },
+      backgroundImage:{
+        'hero-bg': "url('./src/assets/footer-bg2.jpg')"
+      }
     },
   },
   plugins: [],
-}
+};
