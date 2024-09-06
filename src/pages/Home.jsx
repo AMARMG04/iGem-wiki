1d4640e57453e98f98a36d8c62426cae62edd8f3
Pradeep <pradee.sinivas@gmail.com>
2024-09-06 11:15:24 +0530

    push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,141 @@ import Ocean from "../components/Ocean";
 gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
+  const glowStyle = `
+
+
+
+
+body {
+  background:#2EB5E5 ;
+}
+
+.sun { 
+  position: absolute;
+	top:0;
+	left:0;
+	right:0;
+	bottom:0;
+	margin: auto;  
+	width:70px;
+	height:70px;
+	border-radius:50%;	
+	background:white;
+	opacity:0.9;			
+	box-shadow: 0px 0px 40px 15px white;  
+}
+
+.ray_box {
+  position: absolute;
+  margin: auto;
+	top:0px;
+	left:0;
+	right:0;
+	bottom:0;	
+  width:70px;  
+  -webkit-animation: ray_anim 120s linear infinite;
+  animation: ray_anim 120s linear infinite;
+}
+.ray {  
+    background: -webkit-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,0.8) 50%, rgba(255,255,255,0) 100%);
+		background: linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,0.8) 50%, rgba(255,255,255,0) 100%); 
+    margin-left:10px;
+    border-radius:80% 80% 0 0;
+    position:absolute;
+    opacity:0.1;
+}
+
+.ray1 {    
+    height:170px;
+    width:30px;
+   -webkit-transform: rotate(180deg);
+    top:-175px;
+    left: 15px;
+}
+.ray2 {
+    height:100px;
+    width:8px;
+   -webkit-transform: rotate(220deg);
+    top:-90px;
+    left: 75px;
+}
+.ray3 {
+    height:170px;
+    width:50px;
+   -webkit-transform: rotate(250deg);
+    top:-80px;
+    left: 100px;
+}
+.ray4 {
+    height:120px;
+    width:14px;
+   -webkit-transform: rotate(305deg);
+    top:30px;
+    left: 100px;
+}
+.ray5 {
+    height:140px;
+    width:30px;
+   -webkit-transform: rotate(-15deg);
+    top:60px;
+    left: 40px;
+}
+.ray6 {
+    height:90px;
+    width:50px;
+   -webkit-transform: rotate(30deg);
+    top:60px;
+    left: -40px;
+}
+.ray7 {
+    height:180px;
+    width:10px;
+   -webkit-transform: rotate(70deg);
+    top:-35px;
+    left: -40px;
+}
+.ray8 {
+    height:120px;
+    width:30px;
+   -webkit-transform: rotate(100deg);
+    top:-45px;
+    left:-90px;
+}
+.ray9 {
+    height:80px;
+    width:10px;
+   -webkit-transform: rotate(120deg);
+    top:-65px;
+    left:-60px;
+}
+.ray10 {
+    height:190px;
+    width:23px;
+   -webkit-transform: rotate(150deg);
+    top:-185px;
+    left: -60px;
+}
+
+
+@-webkit-keyframes ray_anim { 
+		0% { -webkit-transform: rotate(0deg); transform: rotate(0deg);}    
+    100% { -webkit-transform: rotate(360deg); transform: rotate(360deg);}
+}
+
+  .glow {
+    animation: glow 1s ease-in-out infinite alternate;
+  }
+
+  @keyframes glow {
+    from {
+      box-shadow: 0 0 20px #fff, 0 0 40px #fff, 0 0 60px orange, 0 0 80px orange , 0 0 100px orange, 0 0 120px orange , 0 0 140px orange;
+    }
+    to {
+      box-shadow: 0 0 40px #fff, 0 0 60px orange , 0 0 80px orange, 0 0 100px orange , 0 0 120px orange, 0 0 140px orange , 0 0 160px orange;
+    }
+  }
+
+`;
   const worldRef = useRef(null);
   const plasticsRef = useRef([]);
   const lenisRef = useRef(null);
@@ -141,9 +276,9 @@ const Home = () => {
           boxShadow: "0px 0px 175.5799560546875px 0 #680783, 0px 0px 271.760009765625px 0 #680783, 0px 0px 231.8599853515625px 0 #680783, 0px 0px 65.92999267578125px 0 #680783, 0px 0px 20.97999954223633px 0 #680783, 0px 0px 7.989999771118164px 0 #680783",
         }}>
           <h2 className="text-xl text-center font-quick">DEPRICIATED MARINE ECOSYSTEM VALUE</h2>
-          <p className="text-3xl text-center my-2 font-keania text-yellow-200">$400 Million</p>
+          <p className="text-3xl text-center my-2 font-keania text-orange-200">$400 Million</p>
           <p className="text-3xl text-center my-2 font-keania">To</p>
-          <p className="text-3xl text-center my-2 font-keania text-yellow-200">$2500 Million</p>
+          <p className="text-3xl text-center my-2 font-keania text-orange-200">$2500 Million</p>
         </div>
 
         <div className="absolute right-24 top-[400px] pt-4 xl:pt-10 w-[300px] py-6 h-auto rounded-xl text-white bg-transparent">
@@ -155,12 +290,31 @@ const Home = () => {
       </div>
 
       {/* New Section and Ocean Component */}
-      <section className="new-section">
+      <section className="new-section relative">
         <div className="items-center justify-center h-auto grid grid-cols-2">
           <div className="next-section flex items-center justify-center h-screen px-20 text-white col-span-1">
           </div>
-          <div className="w-[80%] ml-12 col-span-1">
-            <h2 className="text-[45px] text-white font-nohemi_sb mb-4">WHAT ARE THEY?</h2>
+          <div className="w-[80%]  col-span-1">
+            <style>{glowStyle}</style>
+<div className="rounded-full w-[170px] h-[170px] absolute top-12 right-32 bg-white glow">
+<div class="sun">
+    <div class="ray_box">
+        <div class="ray ray1"></div>
+        <div class="ray ray2"></div>
+        <div class="ray ray3"></div>
+        <div class="ray ray4"></div>
+        <div class="ray ray5"></div>
+        <div class="ray ray6"></div>
+        <div class="ray ray7"></div>
+        <div class="ray ray8"></div>
+        <div class="ray ray9"></div>
+        <div class="ray ray10"></div>
+    </div>
+</div>
+</div>
+
+
+            <h2 className="text-[45px] ml-12 text-white font-nohemi_sb mb-4">WHAT ARE THEY?</h2>
             <p className="text-lg text-white leading-relaxed font-nohemi_r">
               Microplastics are small plastic particles less than 5mm in size, which are pervasive in our environment. They come from a variety of sources, including the breakdown of larger plastic debris, microbeads in personal care products, and synthetic fibers from textiles. These tiny particles pose significant risks to marine ecosystems, as they are ingested by marine organisms and can cause physical harm, chemical contamination, and even enter the human food chain.
             </p>
