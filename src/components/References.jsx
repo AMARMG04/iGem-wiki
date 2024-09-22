68be6d7cbcd55f4ca9a28f7928d9d67cc8adc595
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2024-09-22 15:25:22 +0530

    Modified

--- src/components/References.jsx
+++ src/components/References.jsx
@@ -3,11 +3,11 @@
 import React, { useState } from "react";
 import { ChevronDownIcon } from "lucide-react";
 
-export default function References({ references }) {
+export default function References({references}) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="w-full ">
+    <div className="w-full shadow-md">
       <button
         className="w-full font-nohemi_m text-3xl border-l-4 border-indigo-700 pl-6 flex justify-between items-center p-4 font-bold bg-white hover:bg-gray-50 transition-colors"
         onClick={() => setIsOpen(!isOpen)}
@@ -29,19 +29,15 @@ export default function References({ references }) {
             <a href={ref.link} target="_blank">
               <div
                 key={index}
-                className="border-b pb-4 last:border-b-0 p-4 space-y-4 hover:bg-indigo-700 hover:text-white"
+                className="border-b pb-4 p-4 space-y-4 hover:bg-indigo-700 hover:text-white"
               >
-                <h3 className="font-nohemi_m text-2xl">
-                  {ref.title} {ref.year}
-                </h3>
-                <p className="text-md font-inter font-medium">{ref.authors}</p>
+                <h3 className="font-nohemi_m text-2xl">{ref.title} {ref.year}</h3>
+                <p className="text-md font-inter font-medium">
+                  {ref.authors}
+                </p>
                 <div className="flex justify-between font-inter font-semibold">
-                  <p>
-                    {ref.volume !== "NA"
-                      ? `${ref.journal}, ${ref.volume}`
-                      : ref.journal}
-                  </p>
-                  {ref.page !== "NA" && <p>{ref.page}</p>}
+                <p>{ref.volume !== "NA" ? `${ref.journal}, ${ref.volume}` : ref.journal}</p>
+                {ref.page !== "NA" && <p>{ref.page}</p>}
                 </div>
               </div>
             </a>
